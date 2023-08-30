0879260eb3a923aa07868e8b42cb5e3b1db88c49
Fidel Villanueva <fvillanueva.pe@gmail.com>
2023-08-30 17:18:43 -0500

    Learning if and else in Go

---
+++ 06-if-else/main.go
@@ -0,0 +1,42 @@
+package main
+
+// Import three modules
+import (
+	"fmt"
+	"log"
+	"strconv"
+)
+
+func main(){
+	// Define two variables
+	var value_one int =10
+	var value_two int= 20
+
+	// Using if
+	if value_one == value_two {
+		fmt.Println("It's equal")
+	}else{
+		fmt.Println("It is not equal")
+	}
+
+	// Use and
+	if value_one==10 && value_two==20 {
+		fmt.Println("The value one is equal to 10 and values two is equal to 20")
+	}
+
+	// Using or
+	if value_one ==10 || value_two==50 {
+		fmt.Println("One value or two values is equal")
+	}
+
+	// Convert text to number and validate using if and else
+	value, err := strconv.Atoi("53")
+	if err != nil {
+		log.Fatal(err)
+	}else{
+		fmt.Println("The number is: ", value)
+	}
+
+
+
+}
